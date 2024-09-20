57424ccf4beac458479043857d86528b756a9fc5
wkhabou3 <wkhabou33@gmail.com>
2024-09-20 11:41:45 -0400

    just committing before merging with main

--- details/models.py
+++ details/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
+    place_id = models.CharField(max_length=255)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
 
