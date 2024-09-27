0f98ad04d6f508cd174c2e049f850a59a9ede54f
wkhabou3 <wkhabou33@gmail.com>
2024-09-27 12:18:34 -0400

    Added ability to unfavorite restaurants. A few minor fixes needed

--- favorites/urls.py
+++ favorites/urls.py
@@ -1,8 +1,10 @@
 # urls.py
 from django.urls import path
-from .views import add_favorite, user_favorites
+from .views import add_favorite, remove_favorite, user_favorites, get_favorites
 
 urlpatterns = [
     path('addFavorite/', add_favorite, name='add_favorite'),
+    path('removeFavorite/', remove_favorite, name='remove_favorite'),
     path('favorites/', user_favorites, name='favorites'),
+    path('get_favorites/', get_favorites, name='get_favorites'),
 ]
--- favorites/views.py
+++ favorites/views.py
@@ -4,6 +4,7 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 from .models import Favorite
 from django.conf import settings
 
@@ -23,7 +24,24 @@ def add_favorite(request):
     else:
         return JsonResponse({'error': 'Unauthorized'}, status=403)
 
+@require_POST
+def remove_favorite(request):
+    if request.user.is_authenticated:
+        data = json.loads(request.body)
+        place_id = data.get('place_id')
+        name = data.get('name')
 
+        if place_id:
+            try:
+                favorite = Favorite.objects.get(user=request.user, place_id=place_id, name=name)
+                favorite.delete()  # Remove the favorite entry
+                return JsonResponse({'success': True})  # Return success response
+            except Favorite.DoesNotExist:
+                return JsonResponse({'error': 'Favorite not found'}, status=404)
+        else:
+            return JsonResponse({'error': 'place_id is required'}, status=400)
+    else:
+        return JsonResponse({'error': 'Unauthorized'}, status=403)
 
 def user_favorites(request):
     saved_restaurants = Favorite.objects.filter(user=request.user).order_by(
@@ -35,3 +53,10 @@ def user_favorites(request):
         'saved_restaurants': saved_restaurants
     }
     return render(request, 'favorites/favorites.html', context)
+
+def get_favorites(request):
+    if request.user.is_authenticated:
+        favorites = Favorite.objects.filter(user=request.user).values_list('place_id', flat=True)
+        return JsonResponse(list(favorites), safe=False)
+    else:
+        return JsonResponse({'error': 'Unauthorized'}, status=403)
