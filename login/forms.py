e1ca3fefa948c1c9dad7edca0c192d4aa641e2cb
Yuvraj <ydhadwal3@gatech.edu>
2024-09-27 12:04:35 -0400

    made it so that accounts must have unique emails

--- login/forms.py
+++ login/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db import models
 
 
 class LoginForm(forms.Form):
@@ -9,6 +11,15 @@ class LoginForm(forms.Form):
 
 
 class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        # Check if the email already exists in the database
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An account with this email already exists.")
+        return email
