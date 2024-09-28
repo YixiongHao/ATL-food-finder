4ef27553973c71569994ef337588b9cb744c4eec
Yixiong Hao <yixiong_hao@outlook.com>
2024-09-28 17:23:57 -0400

    search side bar now shows cuisine and distance properly

--- search/views.py
+++ search/views.py
@@ -4,9 +4,8 @@ import requests
 from ATL_food_finder.settings import GOOGLE_API_KEY
 from django.conf import settings
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 import rich
-import json
+from math import radians, sin, cos, sqrt, atan2
 
 def search_nearby_restaurants(request):
     api_key = GOOGLE_API_KEY
@@ -57,6 +56,10 @@ def search_nearby_restaurants(request):
                 print("No results found")
                 print(response.json())  # Print the full response for debugging
 
+            for place in results:
+                place['distance'] = calculate_distance(lat, long, place['location']['latitude'], place['location']['longitude'])
+                place['cuisine'] = parse_underscore(place['primaryType'])
+
             filtered_results = [place for place in results if (place.get('rating', 0) >= min_rating)]
 
             context = {'results': filtered_results, 'google_maps_api_key': api_key}
@@ -66,3 +69,19 @@ def search_nearby_restaurants(request):
 
     except requests.RequestException as e:
         return JsonResponse({'error': str(e)}, status=500)
+
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+    R = 3959  # Earth's radius in miles
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    return round(R * c, 1)
+
+def parse_underscore(text):
+    if '_' in text:
+        return ' '.join(text.split('_'))
+    return text
+
